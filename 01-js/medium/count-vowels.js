6ca366104a48e0e28e8dbd63bf56b534745d3060
ANCHITPARATIRA <Anchit.Guria@paratira.ai>
2026-01-30 16:11:27 +0530

    completed-count-vowels

--- 01-js/medium/count-vowels.js
+++ 01-js/medium/count-vowels.js
@@ -7,6 +7,17 @@
 
 function countVowels(str) {
     // Your code here
+    var count = 0;
+    var lowerStr = str.toLowerCase();
+    var vowels = ["a", "e", "i", "o", "u"];
+
+    for(var i=0;i<lowerStr.length; i++){
+      if(vowels.find(v => v === lowerStr[i])){
+        count = count+1;
+      }
+    }
+
+    return count;
 }
 
 module.exports = countVowels;
