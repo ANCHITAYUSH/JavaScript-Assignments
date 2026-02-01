0d2d565b7a76fc21ae7d05780e923d02c28528ba
ANCHITAYUSH <professionalanchit@gmail.com>
2026-02-01 20:37:53 +0530

    completed-read-file-method

--- 01-async-js/easy/3-read-from-file.js
+++ 01-async-js/easy/3-read-from-file.js
@@ -5,3 +5,20 @@
 // Try to do an expensive operation below the file read and see how it affects the output. 
 // Make the expensive operation more and more expensive and see how it affects the output. 
 
+const fs = require('fs')
+
+function readeFile(error, data) {
+    console.log(data);
+}
+
+fs.readFile("read.txt", "utf-8", readeFile);
+
+let sum = 0;
+
+for(let i=1;i<10000000000;i++){
+    sum+=i;
+}
+
+console.log(sum);
+
+
