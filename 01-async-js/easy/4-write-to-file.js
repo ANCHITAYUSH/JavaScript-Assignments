37c1585be7d4878b96f47601771b81cadc925311
ANCHITAYUSH <professionalanchit@gmail.com>
2026-02-02 11:08:18 +0530

    completed-write-file

--- 01-async-js/easy/4-write-to-file.js
+++ 01-async-js/easy/4-write-to-file.js
@@ -2,3 +2,13 @@
 
 // Using the fs library again, try to write to the contents of a file.
 // You can use the fs library to as a black box, the goal is to understand async tasks.
+
+const fs = require("fs");
+
+const data = "You can use the fs library to as a black box, the goal is to understand async tasks."
+
+function callBack(){
+    console.log("File written");
+}
+
+fs.writeFile("write.txt", data, callBack);
